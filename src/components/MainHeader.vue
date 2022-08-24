12f85a4267ea61b9e508cf9692ec43e6e0d2371f
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-08-24 14:22:22 -0400

    Primeiro Componente Vue

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="header">
+        <img class="logo" src="./../assets/logo.png" alt="MfirstVue">
+        <p class="desc">
+            Este é meu primeiro componente em Vue: 24/08/2022.
+        </p>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MainHeader',
+}
+</script>
+
+<style>
+.header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    min-height: 75px;
+    border-bottom: 1px solid #CCC;
+    text-align: center;
+}
+
+.logo {
+    width: 100%;
+    max-width: 100px;
+}
+
+.desc {
+    width: 100%;
+    max-width: 375px;
+    font-weight: 600;
+    color: rgb(75, 75, 75);
+    cursor: default;
+}
+</style>
